b8c6f9e6fa0f1a130b9fce16b379cdeae41889d9
NuMan MirXa <35162466+technofun@users.noreply.github.com>
2019-10-27 12:00:59 +0500

    Update server.js

--- server.js
+++ server.js
@@ -2,5 +2,8 @@ const express = require('express');
 const app = express();
 let port = 5000;
 
+app.get('/',(req,res)=>{
+res.send('server is runing')
+});
 
 app.listen(port,()=> console.log(`server i running on port ${port}`))
